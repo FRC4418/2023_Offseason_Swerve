73d8f67521700a1450315a8c449b20050d3279b4
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-03-29 15:32:28 -0600

    started building swerve subsystem

--- src/main/java/frc/robot/subsystems/swerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerveSubsystem.java
@@ -4,21 +4,62 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Ports;
 
 public class swerveSubsystem extends SubsystemBase {
   /** Creates a new swerveSubsystem. */
+  private final swerveModule fl = new swerveModule(
+    Ports.motorPorts.frontLeftDrive, 
+    Ports.motorPorts.frontLeftTurn, 
+    false, 
+    false, 
+    Ports.encoderPorts.kFrontLeftDriveAbsoluteEncoderPort, 
+    Constants.encoders.kFrontLeftDriveAbsoluteEncoderOffsetRad, 
+    false);
+
+  private final swerveModule fr = new swerveModule(
+    Ports.motorPorts.frontRightDrive, 
+    Ports.motorPorts.frontRightTurn, 
+    false, 
+    false, 
+    Ports.encoderPorts.kFrontRightDriveAbsoluteEncoderPort, 
+    Constants.encoders.kFrontRightDriveAbsoluteEncoderOffsetRad, 
+    false);
+
+  private final swerveModule bl = new swerveModule(
+    Ports.motorPorts.backLeftDrive, 
+    Ports.motorPorts.backLeftTurn,
+    true, 
+    false, 
+    Ports.encoderPorts.kBackLeftDriveAbsoluteEncoderPort, 
+    Constants.encoders.kBackLeftDriveAbsoluteEncoderOffsetRad,
+    false);
+
+  private final swerveModule br = new swerveModule(
+    Ports.motorPorts.backRightDrive,
+    Ports.motorPorts.backRightDrive, 
+    true, 
+    false, 
+    Ports.encoderPorts.kBackRightDriveAbsoluteEncoderPort, 
+    Constants.encoders.kBackRightDriveAbsoluteEncoderOffsetRad, 
+    false);
+
+  private final AHRS gyro = new AHRS();
+  
   public swerveSubsystem() {
-    final swerveModule fl = new swerveModule(
-      Ports.motorPorts.frontLeftTurn, 
-      Ports.motorPorts.frontLeftTurn, 
-      false, 
-      false, 
-      Ports.encoderPorts.kFrontLeftDriveAbsoluteEncoderPort, 
-      Constants.encoders.kFrontLeftDriveAbsoluteEncoderOffsetRad, 
-      false);
+    new Thread(() ->  {
+      try{
+        Thread.sleep(1000);
+        zeroHeading();
+      } catch (Exception e){}
+    }).start();
+  }
+  public void zeroHeading(){
+    gyro.reset();
   }
 
   @Override
