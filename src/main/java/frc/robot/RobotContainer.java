84ed481f60e43f4a8005e0907963e31c2760b4c4
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-03-31 10:23:08 -0600

    i dont even know

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,13 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.AbsoluteDrive;
+import frc.robot.commands.AbsoluteFieldDrive;
+import frc.robot.commands.TeleopDrive;
 import frc.robot.commands.doNothing;
+import frc.robot.subsystems.swerveSubsystem;
+
+import java.io.File;
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
@@ -13,7 +19,10 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.server.PathPlannerServer;
 import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -30,11 +39,47 @@ public class RobotContainer {
 
   private PathPlannerTrajectory traj = PathPlanner.loadPath("Test1", new PathConstraints(1.5, 1.5));
 
+  private final swerveSubsystem driveBase =  new swerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/falcon"));
+
   public RobotContainer() {
     PathPlannerServer.startServer(5811);
     SmartDashboard.putData("Field", field);
     
+    // Configure the trigger bindings
     configureButtonBindings();
+
+    AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(driveBase,
+                                                          // Applies deadbands and inverts controls because joysticks
+                                                          // are back-right positive while robot
+                                                          // controls are front-left positive
+                                                          () -> MathUtil.applyDeadband(driver.getLeftY(),
+                                                                                       0.01),
+                                                          () -> MathUtil.applyDeadband(driver.getLeftX(),
+                                                                                       0.01),
+                                                          () -> -driver.getRightX(),
+                                                          () -> -driver.getRightY(),
+                                                          false);
+
+    AbsoluteFieldDrive closedFieldAbsoluteDrive = new AbsoluteFieldDrive(driveBase,
+                                                                         () ->
+                                                                             MathUtil.applyDeadband(driver.getLeftY(),
+                                                                                                    0.01),
+                                                                         () -> MathUtil.applyDeadband(driver.getLeftX(),
+                                                                                                      0.01),
+                                                                         () -> driver.getLeftX(), false);
+    TeleopDrive simClosedFieldRel = new TeleopDrive(driveBase,
+                                                    () -> MathUtil.applyDeadband(driver.getLeftY(),
+                                                                                 0.01),
+                                                    () -> MathUtil.applyDeadband(driver.getLeftX(),
+                                                                                 0.01),
+                                                    () -> driver.getRightX(), () -> true, false, true);
+    TeleopDrive closedFieldRel = new TeleopDrive(
+        driveBase,
+        () -> MathUtil.applyDeadband(driver.getLeftY(), 0.01),
+        () -> MathUtil.applyDeadband(driver.getLeftX(), 0.01),
+        () -> -driver.getRightX(), () -> true, false, true);
+
+    driveBase.setDefaultCommand(!RobotBase.isSimulation() ? closedAbsoluteDrive : closedFieldAbsoluteDrive);
   }
 
   private void configureButtonBindings(){
