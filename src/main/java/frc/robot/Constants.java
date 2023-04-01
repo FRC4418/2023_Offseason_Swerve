4a8729cb3212a86e89e682aac96346fa48afcbb3
Mihai Nicoara <nicoaram@s.dcsdk12.org>
2023-04-01 13:33:44 -0600

    restart

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.util.Units;
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -19,64 +15,5 @@ import edu.wpi.first.math.util.Units;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-  }
-  public static class moduleConstants{
-    public static final double kPhysicalMaxSpeedPerSeconds = 10.0;
-    public static final double kWheelDiameter = Units.inchesToMeters(4);
-    public static final double kDriveMotorGearRatio = 1 / 8;
-    public static final double kTurningMotorGearRatio = 1 / 8;
-    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameter;
-    public static final double kTurningEncoderRot2Meter = kTurningMotorGearRatio * Math.PI * 2;
-    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
-    public static final double kTurningEncoderRPM2MeterPerSec = kTurningEncoderRot2Meter / 60;
-    public static final double kPTurning = 0.5;
-  }
-  public static class encoders{
-    public static final boolean kFrontLeftDriveEncoderReversed = true;
-    public static final boolean kBackLeftDriveEncoderReversed = true;
-    public static final boolean kFrontRightDriveEncoderReversed = false;
-    public static final boolean kBackRightDriveEncoderReversed = false;
-    
-    public static final boolean kFrontLeftTurningEncoderReversed = true;
-    public static final boolean kBackLeftTurningEncoderReversed = true;
-    public static final boolean kFrontRightTurningEncoderReversed = true;
-    public static final boolean kBackRightTurningEncoderReversed = true;
-
-    public static final boolean kFrontLeftDriveAbsoluteEncoderReversed = false;
-    public static final boolean kBackLeftDriveAbsoluteEncoderReversed = false;
-    public static final boolean kFrontRightDriveAbsoluteEncoderReversed = false;
-    public static final boolean kBackRightDriveAbsoluteEncoderReversed = false;
-
-    public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -0.254;
-    public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = -1.252;
-    public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = -1.816;
-    public static final double kBackRightDriveAbsoluteEncoderOffsetRad = -4.811;
-  }
-  public static class moduleTranslations{
-    public static final Translation2d frontRightTrans = new Translation2d(0.5,0.5);
-    public static final Translation2d frontLeftTrans = new Translation2d(0.5,0.5);
-    public static final Translation2d backRightTrans = new Translation2d(0.5,0.5);
-    public static final Translation2d backLeftTrans = new Translation2d(0.5,0.5);
-  }
-  public static class controllerConstants{
-    public static final double deadBand = 0.3;
-  }
-  public static class DriveConstants{
-    public static double kMaxAccelerationUnitsPerSecond = 1.0;
-    public static double kMaxAngularAccelerationPerSecond = 1.0;
-    public static double kMaxTeleOpSpeed = 2.0;
-    public static double kMaxTeleOpAngularSpeed = 1.5;
-    public static double kEncoderCPR = 2048;
-  }
-  public static class kinematics{
-    public static final double kTrackWidth = Units.inchesToMeters(21);
-    public static final double kWheelBase = Units.inchesToMeters(25);
-    
-    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-      new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-      new Translation2d(kWheelBase/ 2, kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
-      new Translation2d(-kWheelBase / 2, kTrackWidth / 2)
-    );
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,91 +5,59 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.AbsoluteDrive;
-import frc.robot.commands.AbsoluteFieldDrive;
-import frc.robot.commands.TeleopDrive;
-import frc.robot.commands.doNothing;
-import frc.robot.subsystems.swerveSubsystem;
-
-import java.io.File;
-
-import com.pathplanner.lib.PathConstraints;
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.server.PathPlannerServer;
-import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
-
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final Field2d field = new Field2d();
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-  private final AutoGamepad driver = new AutoGamepad(Ports.controllerPorts.driver);
-
-  private PathPlannerTrajectory traj = PathPlanner.loadPath("Test1", new PathConstraints(1.5, 1.5));
-
-  private final swerveSubsystem driveBase =  new swerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/falcon"));
-
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    PathPlannerServer.startServer(5811);
-    SmartDashboard.putData("Field", field);
-    
     // Configure the trigger bindings
-    configureButtonBindings();
-
-    AbsoluteDrive closedAbsoluteDrive = new AbsoluteDrive(driveBase,
-                                                          // Applies deadbands and inverts controls because joysticks
-                                                          // are back-right positive while robot
-                                                          // controls are front-left positive
-                                                          () -> MathUtil.applyDeadband(driver.getLeftY(),
-                                                                                       0.01),
-                                                          () -> MathUtil.applyDeadband(driver.getLeftX(),
-                                                                                       0.01),
-                                                          () -> -driver.getRightX(),
-                                                          () -> -driver.getRightY(),
-                                                          false);
-
-    AbsoluteFieldDrive closedFieldAbsoluteDrive = new AbsoluteFieldDrive(driveBase,
-                                                                         () ->
-                                                                             MathUtil.applyDeadband(driver.getLeftY(),
-                                                                                                    0.01),
-                                                                         () -> MathUtil.applyDeadband(driver.getLeftX(),
-                                                                                                      0.01),
-                                                                         () -> driver.getLeftX(), false);
-    TeleopDrive simClosedFieldRel = new TeleopDrive(driveBase,
-                                                    () -> MathUtil.applyDeadband(driver.getLeftY(),
-                                                                                 0.01),
-                                                    () -> MathUtil.applyDeadband(driver.getLeftX(),
-                                                                                 0.01),
-                                                    () -> driver.getRightX(), () -> true, false, true);
-    TeleopDrive closedFieldRel = new TeleopDrive(
-        driveBase,
-        () -> MathUtil.applyDeadband(driver.getLeftY(), 0.01),
-        () -> MathUtil.applyDeadband(driver.getLeftX(), 0.01),
-        () -> -driver.getRightX(), () -> true, false, true);
-
-    driveBase.setDefaultCommand(!RobotBase.isSimulation() ? closedAbsoluteDrive : closedFieldAbsoluteDrive);
+    configureBindings();
   }
 
-  private void configureButtonBindings(){
-    
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
-
-
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
-
-    return new doNothing();
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
   }
 }
